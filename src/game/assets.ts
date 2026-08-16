2a2593b37e07401f3293c0670a267c1792fc87c6
LangLangGaming <vbt2050@gmail.com>
2026-08-16 19:28:23 +0800

    fix: fixed 404 errors on deployment

--- src/game/assets.ts
+++ src/game/assets.ts
@@ -1,3 +1,10 @@
+import birdPngUrl from '../assets/bird.png'
+import pipeHeadUrl from '../assets/pipeHead.png'
+import pipeBodyUrl from '../assets/pipeBody.png'
+import backgroundUrl from '../assets/Background3.png'
+import tilesUrl from '../assets/tiles.png'
+import heartUrl from '../assets/heart.png'
+
 export function loadImage(src: string): Promise<HTMLImageElement> {
   return new Promise((resolve, reject) => {
     const img = new Image()
@@ -8,17 +15,16 @@ export function loadImage(src: string): Promise<HTMLImageElement> {
 }
 
 export async function loadGameAssets() {
-  const [birdPng, bird2x, pipeHead, pipeBody, background, floor, heart] = await Promise.allSettled([
-    loadImage('/src/assets/bird.png'),
-    loadImage('/src/assets/bird@2x.png'),
-    loadImage('/src/assets/pipeHead.png'),
-    loadImage('/src/assets/pipeBody.png'),
-    loadImage('/src/assets/Background3.png'),
-    loadImage('/src/assets/tiles.png'),
-    loadImage('/src/assets/heart.png'),
+  const [birdPng, pipeHead, pipeBody, background, floor, heart] = await Promise.allSettled([
+    loadImage(birdPngUrl),
+    loadImage(pipeHeadUrl),
+    loadImage(pipeBodyUrl),
+    loadImage(backgroundUrl),
+    loadImage(tilesUrl),
+    loadImage(heartUrl),
   ])
 
-  const birdImg = bird2x.status === 'fulfilled' && window.devicePixelRatio >= 2 ? bird2x.value : birdPng.status === 'fulfilled' ? birdPng.value : null
+  const birdImg = birdPng.status === 'fulfilled' ? birdPng.value : null
 
   return {
     birdImg,
